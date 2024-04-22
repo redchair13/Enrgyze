3109e380ff16359b476515f215b358f0719e1eb3
redchair13 <muwahidseraj11@gmail.com>
2024-04-22 17:51:16 -0400

    schema

--- server/EnergyDrinkSchema.js
+++ server/EnergyDrinkSchema.js
@@ -6,11 +6,11 @@ const EnergyDrinkSchema = new mongoose.Schema({
     pictureIDs: Object, // this will be an array of pictures - PICTURE - DO LATER - prolly use GridFS or a link to picture
     description: String,
     ratings: [Number], // we will have to calculate the average every time we want to display it
-    // commentIDs: Object, // this will be an array of all the comments
     upvoteCount: Number,
     tagIDs: String, // array of tags
     caffieneContent: Number,
-    nutritionFacts: Object, // PICTURE - DO LATER
+    calories: Number,
+    sugar: Number,
     dateAdded: Number,
     dateLastModified: Number,
     ownerUserID: mongoose.Schema.Types.ObjectId
